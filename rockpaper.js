d3b12f1faaa622259c2a3ae2597dc0e7e8c0c18f
Carl Laguerre <gglaguerre1@gmail.com>
2024-11-05 20:46:30 -0500

    wrote logic to get computer choice

--- rockpaper.js
+++ rockpaper.js
@@ -1,1 +1,19 @@
-console.log("testing");
+console.log("hi");
+
+function getComputerChoice() {
+  let randomCpuChoice = ["rock", "paper", "scissors"];
+  let computerChoice = Math.floor(Math.random() * randomCpuChoice.length);
+  //add switch statement
+  switch (computerChoice) {
+    case 0:
+      return randomCpuChoice[0];
+      break;
+    case 1:
+      return randomCpuChoice[1];
+      break;
+    case 2:
+      return randomCpuChoice[2];
+      break;
+  }
+}
+//console.log(getComputerChoice());

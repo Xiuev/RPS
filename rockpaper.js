214a8ba316d4910d86d8a481df5b2d9a339ea43c
Carl Laguerre <gglaguerre1@gmail.com>
2024-11-05 21:11:32 -0500

    created getHumanChoice function -- gets userinput

--- rockpaper.js
+++ rockpaper.js
@@ -1,5 +1,26 @@
 console.log("hi");
 
+function getHumanChoice() {
+  //arry?
+  let humanChoice = prompt("Rock Paper or Scissor?");
+  let humanResult = humanChoice.toLowerCase();
+  switch (humanResult) {
+    case "rock":
+      return "rock";
+      break;
+    case "paper":
+      return "paper";
+      break;
+    case "scissor":
+      return "scissor";
+      break;
+
+    default:
+      return "invalid choice";
+  }
+}
+//console.log(getHumanChoice());
+
 function getComputerChoice() {
   let randomCpuChoice = ["rock", "paper", "scissors"];
   let computerChoice = Math.floor(Math.random() * randomCpuChoice.length);

fc3efcff5faa6c6290870eb99b44f5f58e4ed211
Carl Laguerre <gglaguerre1@gmail.com>
2024-11-09 16:14:27 -0500

    created playgame function which simulates a single round

--- rockpaper.js
+++ rockpaper.js
@@ -1,5 +1,44 @@
 console.log("hi");
 
+//const humanSelection = getHumanChoice;
+//const computerSelection = getComputerChoice;
+function playGame() {
+  let humanSelection = getHumanChoice();
+  let computerSelection = getComputerChoice();
+  let humanScore = 0;
+  let computerScore = 0;
+  function playRound(humanChoice, computerChoice) {
+    //DONT FORGET TO INCREASE SCORE OF WINNER
+    if (humanChoice === "paper" && computerChoice === "rock") {
+      humanScore += 1;
+      return "You win! Paper beats Rock!";
+    } else if (humanChoice === "rock" && computerChoice === "scissor") {
+      humanScore += 1;
+      return "You win! Rock beats Scissors";
+    } else if (humanChoice === "scissor" && computerChoice === "paper") {
+      humanScore += 1;
+      return "You win! Scissor beats Paper!";
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+      computerScore += 1;
+      return "You lose! Paper beats Rock";
+    } else if (computerChoice === "rock" && humanChoice === "scissor") {
+      computerScore += 1;
+      return "You lose! rock beats scissor";
+    } else if (computerChoice === "scissor" && humanChoice === "paper") {
+      computerScore += 1;
+      return "You Lose! scissor beats paper";
+    } else if (computerChoice === humanChoice) {
+      computerScore += 1;
+      return "DRAW!";
+    }
+  }
+  //call playround 5 times using for loop?(add loop in playRound instead?)
+  for (let i = 0; i < 2; i++) {
+    playRound(humanSelection, computerSelection);
+  }
+  return humanSelection;
+}
+
 function getHumanChoice() {
   //arry?
   let humanChoice = prompt("Rock Paper or Scissor?");
@@ -38,3 +77,5 @@ function getComputerChoice() {
   }
 }
 //console.log(getComputerChoice());
+//console.log(playRound(getHumanChoice(), getComputerChoice()));
+//console.log(playGame());
